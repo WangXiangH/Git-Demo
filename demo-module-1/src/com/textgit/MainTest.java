0a9265ef479b54593ad8061f7264690c6db47124
WangXiangH <1589336904@qq.com>
2020-12-08 16:27:08 +0800

    尝试第二次提交

--- demo-module-1/src/com/textgit/MainTest.java
+++ demo-module-1/src/com/textgit/MainTest.java
@@ -2,6 +2,6 @@ package com.textgit;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("hellow github");
+        System.out.println("hellow git");
     }
 }
