53acccffe739c29acfc5f5e9ea3063173d6ad6b4
WangXiangH <1589336904@qq.com>
2020-12-09 19:30:44 +0800

    冲突解决

--- demo-module-1/src/com/textgit/MainTest.java
+++ demo-module-1/src/com/textgit/MainTest.java
@@ -3,5 +3,6 @@ package com.textgit;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hellow git");
+        System.out.println("hellow test");
     }
 }
